b69fa95f52eb93c8c98e8343a1ecd77552a327f2
Vakhobdzhon Vakhobov <vxkhob7@gmail.com>
2025-03-18 03:52:33 -0700

    feat: voting contract implementation vers. 1.0

--- nft_id_contract/programs/nft_id_contract/src/lib.rs
+++ nft_id_contract/programs/nft_id_contract/src/lib.rs
@@ -10,7 +10,7 @@ use anchor_spl::{
 use anchor_spl::token::spl_token as spl_token_3_5;
 use solana_program::hash::hash;
 
-declare_id!("GgLTHPo25XiFsQJAkotD3KPiyMFeypJhUSx4UVcxfjcj");
+declare_id!("G1AMtv7feMb51dTC2i2JhTMfBfBKc5HUomGj1szPaE74");
 
 const BLOCKVOTE_NAME: &str = "BlockVote ID";
 const BLOCKVOTE_SYMBOL: &str = "BVID";
@@ -178,6 +178,46 @@ pub mod nft_id_contract {
         msg!("⏳ Token expired.");
         Ok(())
     }
+
+    //************ VOTING CONTRACT ************//
+
+    pub fn initialize_poll(ctx: Context<InitializePoll>, 
+                _poll_id: u64, 
+                start_time: u64, 
+                end_time: u64,
+                name: String,
+                description: String) -> Result<()> {
+            ctx.accounts.poll_account.poll_name = name;
+            ctx.accounts.poll_account.poll_description = description;
+            ctx.accounts.poll_account.poll_voting_start = start_time;
+            ctx.accounts.poll_account.poll_voting_end = end_time;
+            Ok(())
+    }
+
+    pub fn initialize_candidate(ctx: Context<InitializeCandidate>, 
+                _poll_id: u64, 
+                candidate: String) -> Result<()> {
+            ctx.accounts.candidate_account.candidate_name = candidate;
+            ctx.accounts.poll_account.poll_option_index += 1;
+            Ok(())
+    }
+
+    pub fn vote(ctx: Context<Vote>, _poll_id: u64, _candidate: String) -> Result<()> {
+        let candidate_account = &mut ctx.accounts.candidate_account;
+        let current_time = Clock::get()?.unix_timestamp;
+
+        if current_time > (ctx.accounts.poll_account.poll_voting_end as i64) {
+            return Err(ErrorCode::VotingEnded.into());
+        }
+
+        if current_time <= (ctx.accounts.poll_account.poll_voting_start as i64) {
+            return Err(ErrorCode::VotingNotStarted.into());
+        }
+
+        candidate_account.candidate_votes += 1;
+
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -303,4 +343,93 @@ pub enum ErrorCode {
     AlreadyMinted,
     #[msg("This identity has already been registered.")]
     DuplicateIdentity,
+    #[msg("Voting has not started yet")]
+    VotingNotStarted,
+    #[msg("Voting has ended")]
+    VotingEnded,
 }
+
+
+
+
+
+
+//************************** VOTING CONTRACT **************************//
+#[derive(Accounts)]
+#[instruction(poll_id: u64)]
+pub struct InitializePoll<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        init_if_needed,
+        payer = signer,
+        space = 8 + PollAccount::INIT_SPACE,
+        seeds = [b"poll".as_ref(), poll_id.to_le_bytes().as_ref()],
+        bump
+    )]
+    pub poll_account: Account<'info, PollAccount>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(poll_id: u64, candidate: String)]
+pub struct InitializeCandidate<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    pub poll_account: Account<'info, PollAccount>,
+
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + CandidateAccount::INIT_SPACE,
+        seeds = [poll_id.to_le_bytes().as_ref(), candidate.as_ref()],
+        bump
+    )]
+    pub candidate_account: Account<'info, CandidateAccount>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(poll_id: u64, candidate: String)]
+pub struct Vote<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [b"poll".as_ref(), poll_id.to_le_bytes().as_ref()],
+        bump,
+    )]
+    pub poll_account: Account<'info, PollAccount>,
+
+    #[account(
+        mut,
+        seeds = [poll_id.to_le_bytes().as_ref(), candidate.as_ref()],
+        bump)]
+    pub candidate_account: Account<'info, CandidateAccount>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct CandidateAccount {
+    #[max_len(32)]
+    pub candidate_name: String,
+    pub candidate_votes: u64,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct PollAccount{
+    #[max_len(32)]
+    pub poll_name: String,
+    #[max_len(280)]
+    pub poll_description: String,
+    pub poll_voting_start: u64,
+    pub poll_voting_end: u64,
+    pub poll_option_index: u64,
+}
+
